603cbd5606ca44b6f85fa0e9a61bcf835a8c41e9
Vignesh969696 <ks2vignesh@gmail.com>
2025-10-24 14:36:11 +0530

    model.py

--- model.py
+++ model.py
@@ -4,13 +4,15 @@ import torch.nn as nn
 class DepressionMLP(nn.Module):
     def __init__(self, input_dim):
         super(DepressionMLP, self).__init__()
+
         self.model = nn.Sequential(
             nn.Linear(input_dim, 64),
             nn.ReLU(),
+            nn.Dropout(0.3),
             nn.Linear(64, 32),
             nn.ReLU(),
-            nn.Linear(32, 1)  # Output for binary classification
+            nn.Linear(32, 1)
         )
 
     def forward(self, x):
-        return self.model(x).squeeze(-1)  # Squeeze for BCEWithLogitsLoss
+        return self.model(x)
