6195e35b510a97ecb525193a51e78049143b2644
Vignesh969696 <ks2vignesh@gmail.com>
2025-10-24 15:58:57 +0530

    model.py

--- model.py
+++ model.py
@@ -6,13 +6,17 @@ class DepressionMLP(nn.Module):
         super(DepressionMLP, self).__init__()
 
         self.model = nn.Sequential(
-            nn.Linear(input_dim, 64),
+            nn.Linear(input_dim, 128),
             nn.ReLU(),
             nn.Dropout(0.3),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Dropout(0.2),
             nn.Linear(64, 32),
             nn.ReLU(),
-            nn.Linear(32, 1)
+            nn.Linear(32,1)
         )
 
     def forward(self, x):
         return self.model(x)
+
